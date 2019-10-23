41ea6d44ff8f46b3f8ce6b05acc0395426603d4e
Dilip <dilipkr7587@gmail.com>
2019-10-23 12:52:31 +0530

    Updated app test

--- src/test/java/com/parking/Client/ParkingAppTest.java
+++ src/test/java/com/parking/Client/ParkingAppTest.java
@@ -7,14 +7,10 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class ParkingAppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class ParkingAppTest {
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
 }
