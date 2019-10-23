27a64e0f30b59f244adcfa00c223fa63bd85f906
Dilip <dilipkr7587@gmail.com>
2019-10-23 12:36:45 +0530

    Updated app

--- src/main/java/com/parking/Client/ParkingApp.java
+++ src/main/java/com/parking/Client/ParkingApp.java
@@ -1,13 +1,12 @@
 package com.parking.Client;
 
 /**
- * Hello world!
- *
- */
+ Author: Dilip Kumar
+ **/
 public class ParkingApp 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );     
     }
 }
