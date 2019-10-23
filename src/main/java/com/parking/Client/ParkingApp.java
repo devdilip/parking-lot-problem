e5241c39c1d8da5f473ee3445ea09da0066b4039
Dilip <dilipkr7587@gmail.com>
2019-10-23 12:14:24 +0530

    Added initial file

---
+++ src/main/java/com/parking/Client/ParkingApp.java
@@ -0,0 +1,13 @@
+package com.parking.Client;
+
+/**
+ * Hello world!
+ *
+ */
+public class ParkingApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/parking/Client/ParkingAppTest.java
@@ -0,0 +1,20 @@
+package com.parking.Client;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ParkingAppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
